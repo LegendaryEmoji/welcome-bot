bdb84e4b731a2e5faa0361de391940f8a71312fa
Legendary Emoji <60050010+LegendaryEmoji@users.noreply.github.com>
2021-02-13 21:34:27 +0500

    Update help.js

--- commands/Other/help.js
+++ commands/Other/help.js
@@ -1,6 +1,6 @@
 const { Default_Prefix, Color, Support } = require("../../config.js");
 const Discord = require("discord.js");
-const db = require("wio.db");
+const db = require("old-wio.db");
 
 module.exports = {
   name: "help",
